f27e60e668098d04d38ccb5e15dabcf8441b9f64
Steve Ernstberger <steve32285@gmail.com>
2025-05-30 08:05:51 -0400

    Refactor ConnectedTextInput component to integrate react-hook-form and MUI
    - Updated ConnectedTextInput to utilize react-hook-form's Controller for better form handling.
    - Enhanced component structure by adding FormControl, InputLabel, and Input from MUI for improved UI consistency.
    - Included error handling with FormHelperText to display validation messages.
    - Updated props interface to extend InputProps and ControllerProps, ensuring proper type definitions.

--- libs/form/src/lib/ConnectedTextInput/ConnectedTextInput.tsx
+++ libs/form/src/lib/ConnectedTextInput/ConnectedTextInput.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import { Controller, ControllerProps } from 'react-hook-form';
+import {
+  FormControl,
+  FormHelperText,
+  Input,
+  InputLabel,
+  InputProps,
+} from '@mui/material';
 
-/* eslint-disable-next-line */
-export interface ConnectedTextInputProps {}
-
-export function ConnectedTextInput(props: ConnectedTextInputProps) {
-  return (
-    <div>
-      <h1>Welcome to ConnectedTextInput!</h1>
-    </div>
-  );
+export interface ConnectedTextInputProps
+  extends Omit<InputProps, 'name' | 'defaultValue'>,
+    ControllerProps {
+  name: string;
+  label: string;
 }
 
-export default ConnectedTextInput;
+export function ConnectedTextInput({
+  name,
+  label,
+  rules,
+  ...props
+}: ConnectedTextInputProps) {
+  return (
+    <Controller
+      name={name}
+      render={({ field, fieldState }) => (
+        <FormControl>
+          <InputLabel htmlFor={name}>{label}</InputLabel>
+          <Input {...field} {...props} />
+          {fieldState.error && (
+            <FormHelperText>{fieldState.error.message}</FormHelperText>
+          )}
+        </FormControl>
+      )}
+      rules={rules}
+    />
+  );
+}
---
+++ libs/form/src/lib/FORM_REQUIREMENTS.md
@@ -0,0 +1,16 @@
+# Form Requirements
+
+## Requirements
+
+- [ ] Use react hook form Controller
+- [ ] Use react hook form rules for validation (required, min, max, etc.)
+- [ ] can always pass a custom zod validation function
+- [ ] All inputs need to be wrapped in a form
+- [ ] cant add just spaces to the input - this should fail validation
+- [ ] use hook form formProvider; no need to pass control to the component
+- [ ] use zod for validation
+
+### ConnectedTextInput Tests
+
+- [ ] should render inside a form
+- [ ] should render an input with the correct label
